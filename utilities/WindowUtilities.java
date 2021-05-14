92fbd0b72dbe7beab7312adc188d049dd3e3513b
C0nf1cker <56350482+C0nf1cker@users.noreply.github.com>
2021-05-14 15:28:02 +0200

    First steps with resizeable screen. Still working on...

---
+++ utilities/WindowUtilities.java
@@ -0,0 +1,30 @@
+package utilities;
+
+public class WindowUtilities {
+    private static int width;
+    private static int height;
+
+    public static int getWidth() {
+        return width;
+    }
+
+    public static int getHeight() {
+        return height;
+    }
+
+    public static void setWidth(int w) {
+        width = w;
+    }
+
+    public static void setHeight(int h) {
+        height = h;
+    }
+
+    public static int resizeVerticalDimension(int dimension) {
+        return width / dimension;
+    }
+
+    public static int resizeHorizontalDimension(int dimension) {
+        return height / dimension;
+    }
+}
